a6fe1ad7e59aba40210c11b7a3d584ff55f0ef71
Kayla Gaynor <kgcht@umsystem.edu>
2026-03-14 15:57:05 -0500

    Refactor variable declarations in worker.c

--- worker.c
+++ worker.c
@@ -7,24 +7,32 @@
 #include "shared.h"
 
 int main(int argc, char *argv[]) {
+  int lifeSeconds, lifeNano;
+  int termSeconds, termNano;
+  int done;
+  int msgCount;
+  SimClock *sysClock;
+  int shmId, msgId;
+  Message msg; 
+
   if (argc != 3) {
         fprintf(stderr, "Usage: worker <seconds> <nanoseconds>\n");
         return 1;
   }
 
-  int lifeSeconds = atoi(argv[1]);
-  int lifeNano = atoi(argv[2]);
+  lifeSeconds = atoi(argv[1]);
+  lifeNano = atoi(argv[2]);
 
-  int shmId = shmget(SHM_KEY, sizeof(SimClock), 0666);
+  shmId = shmget(SHM_KEY, sizeof(SimClock), 0666);
   if (shmId == -1) { perror("shmget"); return 1; }
-  SimClock *sysClock = (SimClock *)shmat(shmId, NULL, 0);
+  sysClock = (SimClock *)shmat(shmId, NULL, 0);
   if (sysClock == (void *)-1) { perror("shmat"); return 1; }
 
-  int msgId = msgget(MSG_KEY, 0666);
+  msgId = msgget(MSG_KEY, 0666);
   if (msgId == -1) { perror("msgget"); return 1; }
 
-  int termSeconds = sysClock -> seconds + lifeSeconds;
-  int termNano = sysClock -> nanoseconds + lifeNano;
+  termSeconds = sysClock -> seconds + lifeSeconds;
+  termNano = sysClock -> nanoseconds + lifeNano;
   if (termNano >= 1000000000) {
         termSeconds++;
         termNano -= 1000000000;
@@ -34,9 +42,32 @@ int main(int argc, char *argv[]) {
   printf("TermTimeS: %d TermTimeNano: %d\n", termSeconds, termNano);
   printf("--Just Starting\n");
 
+  done = 0;
+  msgCount = 0;
 
+  do {
+        msgrcv(msgId, &msg, sizeof(msg) - sizeof(long), getpid(), 0);
+        msgCount++;
+
+        if (sysClock -> seconds > termSeconds || (sysClock -> seconds == termSeconds && sysClock -> nanoseconds >= termNano)) {
+        done = 1;
+        }
+
+        printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d\n", getpid(), getppid(), sysClock -> seconds, sysClock -> nanoseconds);
+        printf("TermTimeS: %d TermTimeNano: %d\n", termSeconds, termNano);
+
+        if (done) {
+          printf("--Terminating after sending message back to oss after %d received messages.\n", msgCount);
+        } else {
+          printf("--%d message received from oss\n", msgCount);
+        }
+
+        msg.mtype = getppid();
+        msg.value = done ? 0 : 1;
+        msgsnd(msgId, &msg, sizeof(msg) - sizeof(long), 0);
+
+  } while (!done);
 
   shmdt(sysClock);
   return 0;
 }
-
